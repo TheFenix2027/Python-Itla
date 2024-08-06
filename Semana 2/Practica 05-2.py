8ce230dea01732dfaba6058a1e91b024b4a4eab8
TheFenix2027 <95001663+TheFenix2027@users.noreply.github.com>
2024-08-05 22:12:28 -0400

    Update practica 05-2

--- Semana 2/Practica 05-2.py
+++ Semana 2/Practica 05-2.py
@@ -1,15 +1,18 @@
 #Numeros amigos
-numero1=int(input("Ingrese el primer numero "))
-numero2=int(input("Ingrese el segundo numero "))
-x=2
-suma=0
+try:
+    numero1=int(input("Ingrese el primer numero "))
+    numero2=int(input("Ingrese el segundo numero "))
+    x=2
+    suma=0
 
-while x<=numero1:
-    if numero1 % x==0:
-        suma += numero1//x
-    x+=1
+    while x<=numero1:
+        if numero1 % x==0:
+            suma += numero1//x
+        x+=1
 
-if suma==numero2:
-    print("Estos numeros son amigos")
-else:
-    print("Estos numeros NO son amigos")
+    if suma==numero2:
+        print("Estos numeros son amigos")
+    else:
+        print("Estos numeros NO son amigos")
+except Exception as error:
+    print(f"Ha ocurrido un error! {error}")
