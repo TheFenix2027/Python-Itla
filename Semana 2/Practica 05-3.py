bcad270f9a146229d176dbbce71725867790c8d2
TheFenix2027 <95001663+TheFenix2027@users.noreply.github.com>
2024-08-05 22:12:40 -0400

    Cadena de Fibonacci

--- Semana 2/Practica 05-3.py
+++ Semana 2/Practica 05-3.py
@@ -1,14 +1,17 @@
 #Cadena de fibonacci
-n=int(input("Ingrese un numero para la sucesion de Fibonacci "))
-a=0
-b=1
-c=1
+try:
+    n=int(input("Ingrese un numero para la sucesion de Fibonacci "))
+    a=0
+    b=1
+    c=1
 
-while c<=n:
-    if c % 2==1:
-        print(a, end=",")
-        a+=b
-    else:
-        print(b, end=",")
-        b+=a
-    c+=1
+    while c<=n+1:
+        if c % 2==1:
+            print(a, end=",")
+            a+=b
+        else:
+            print(b, end=",")
+            b+=a
+        c+=1
+except Exception as error:
+    print(f"Ha ocurrido un error! {error}")
