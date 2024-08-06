c1ba088b2788d392ff1ca872e11c818e78da8300
TheFenix2027 <95001663+TheFenix2027@users.noreply.github.com>
2024-08-05 22:12:13 -0400

    Update practica 05-1

--- Semana 2/Practica 05-1.py
+++ Semana 2/Practica 05-1.py
@@ -1,13 +1,17 @@
 #Numeros perfectos
-numero=int(input("Ingrese un numero "))
-x=2
-suma=0
+try:
+    numero=int(input("Ingrese un numero "))
+    x=2
+    suma=0
 
-while x<=numero:
-    if numero % x==0:
-        suma += numero//x
-    x+=1
-if suma==numero:
-    print("El numero es perfecto")
-else:
-    print("El numero no es perfecto")
+    while x<=numero:
+        if numero % x==0:
+            suma += numero//x
+        x+=1
+    if suma==numero:
+        print("El numero es perfecto")
+    else:
+        print("El numero NO es perfecto")
+    print("Gracias por participar")
+except Exception as error:
+    print(f"Ha ocurrido un error! {error}")
